460c917c335127ade271c11ed834e92f6ea2549b
mikulajan180 <mikulajan180@gmail.com>
2020-04-21 12:56:54 +0200

    Added Basic Syntax

---
+++ TraderTool/ItemListARMA3V.hpp
@@ -0,0 +1,343 @@
+//////////////////////////////////////////////////////////////////////////////
+// Arma 3 SapphireExile by Sapphire & stv666
+// www.sapphire.sk
+//////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////
+// Weapons Info
+//////////////////////////////////////////////////////////////////////////////
+
+// MK19 = Gerenade Manual Turret
+// AGS-30 = Russian Gerenede Manual Turrent
+// CROWS = USA/ENG Auto Turret
+//////////////////////////////////////////////////////////////////////////////
+// Respect Settings
+//////////////////////////////////////////////////////////////////////////////
+/*
+1 - 0
+2 - 20 000
+3 - 40 000
+4 - 60 000
+5 - 80 000
+6 - 100 000
+*/
+
+/*
+Example of class name Syntax
+	class B_GEN_Offroad_01_gen_F				{ quality = 1; price = 350000; }; // GAMENAME,SKIN,WEAPON1,WEAPON2,WEAPON3
+*/
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Cars Unarmed
+///////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Cars Armed
+///////////////////////////////////////////////////////////////////////////////
+
+class CUP_B_M1129_MC_MK19_Desert				{ quality = 3; price = 350000; }; // Original Meno Mk
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Trucks Unarmed
+///////////////////////////////////////////////////////////////////////////////
+class B_G_Van_01_fuel_F { quality = 1; price = 30000; };
+class B_G_Van_01_transport_F { quality = 1; price = 20000; };
+class B_Truck_01_Repair_F { quality = 1; price = 25000; };
+class B_Truck_01_ammo_F { quality = 1; price = 25000; };
+class B_Truck_01_box_F { quality = 1; price = 20000; };
+class B_Truck_01_covered_F { quality = 1; price = 20000; };
+class B_Truck_01_fuel_F { quality = 1; price = 25000; };
+class B_Truck_01_medical_F { quality = 1; price = 20000; };
+class B_Truck_01_mover_F { quality = 1; price = 20000; };
+class B_Truck_01_transport_F { quality = 1; price = 20000; };
+class C_IDAP_Offroad_01_F { quality = 1; price = 20000; };
+class C_IDAP_Offroad_02_unarmed_F { quality = 1; price = 20000; };
+class C_IDAP_Truck_02_F { quality = 1; price = 20000; };
+class C_IDAP_Truck_02_transport_F { quality = 1; price = 20000; };
+class C_IDAP_Truck_02_water_F { quality = 1; price = 20000; };
+class C_Truck_02_box_F { quality = 1; price = 20000; };
+class C_Truck_02_covered_F { quality = 1; price = 20000; };
+class C_Truck_02_fuel_F { quality = 1; price = 30000; };
+class C_Truck_02_transport_F { quality = 1; price = 20000; };
+class C_Van_01_box_F { quality = 1; price = 20000; };
+class C_Van_01_box_red_F { quality = 1; price = 20000; };
+class C_Van_01_box_white_F { quality = 1; price = 20000; };
+class C_Van_01_fuel_F { quality = 1; price = 30000; };
+class C_Van_01_fuel_red_F { quality = 1; price = 30000; };
+class C_Van_01_fuel_red_v2_F { quality = 1; price = 30000; };
+class C_Van_01_fuel_white_F { quality = 1; price = 30000; };
+class C_Van_01_fuel_white_v2_F { quality = 1; price = 30000; };
+class C_Van_01_transport_F { quality = 1; price = 20000; };
+class C_Van_01_transport_red_F { quality = 1; price = 20000; }; //VAN pickup Red
+class C_Van_01_transport_white_F { quality = 1; price = 20000; };
+class I_G_Offroad_01_F { quality = 1; price = 20000; };
+class I_G_Offroad_01_armed_F { quality = 1; price = 55000; };
+class I_G_Van_01_fuel_F { quality = 1; price = 30000; };
+class I_G_Van_01_transport_F { quality = 1; price = 20000; };
+class I_Truck_02_ammo_F { quality = 1; price = 58000; };
+class I_Truck_02_box_F { quality = 1; price = 20000; };
+class I_Truck_02_covered_F { quality = 1; price = 20000; };
+class I_Truck_02_fuel_F { quality = 1; price = 30000; };
+class I_Truck_02_medical_F { quality = 1; price = 20000; };
+class I_Truck_02_transport_F { quality = 1; price = 20000; };
+class O_G_Offroad_01_F { quality = 1; price = 18000; };
+class O_G_Offroad_01_armed_F { quality = 1; price = 55000; };
+class O_G_Offroad_01_repair_F { quality = 1; price = 40000; };
+class O_G_Van_01_fuel_F { quality = 1; price = 30000; };
+class O_G_Van_01_transport_F { quality = 1; price = 20000; };
+class O_Truck_02_Ammo_F { quality = 1; price = 60000; };
+class O_Truck_02_box_F { quality = 1; price = 20000; };
+class O_Truck_02_covered_F { quality = 1; price = 20000; };
+class O_Truck_02_fuel_F { quality = 1; price = 30000; };
+class O_Truck_02_medical_F { quality = 1; price = 20000; };
+class O_Truck_02_transport_F { quality = 1; price = 20000; };
+class O_Truck_03_ammo_F { quality = 1; price = 60000; };
+class O_Truck_03_covered_F { quality = 1; price = 20000; };
+class O_Truck_03_device_F { quality = 1; price = 33000; };
+class O_Truck_03_fuel_F { quality = 1; price = 30000; };
+class O_Truck_03_medical_F { quality = 1; price = 18000; };
+class O_Truck_03_repair_F { quality = 1; price = 30000; };
+class O_Truck_03_transport_F { quality = 1; price = 20000; };
+
+///////////////////////////////////////////////////////////////////////////////
+// Truck Armed
+///////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Tracked Unarmed
+///////////////////////////////////////////////////////////////////////////////
+class B_APC_Tracked_01_AA_F { quality = 6; price = 400000; };
+class B_APC_Tracked_01_CRV_F { quality = 6; price = 400000; };
+class B_APC_Tracked_01_rcws_F { quality = 6; price = 400000; };
+class B_APC_Wheeled_01_cannon_F { quality = 6; price = 400000; };
+class B_MBT_01_TUSK_F { quality = 6; price = 1000000; };
+class B_MBT_01_arty_F { quality = 6; price = 5000000; };
+class B_MBT_01_cannon_F { quality = 6; price = 1000000; };
+class B_MBT_01_mlrs_F { quality = 6; price = 6000000; };
+class I_APC_Wheeled_03_cannon_F { quality = 6; price = 500000; };
+class I_APC_tracked_03_cannon_F { quality = 6; price = 400000; };
+class I_MBT_03_cannon_F { quality = 6; price = 1000000; };
+class O_APC_Tracked_02_AA_F { quality = 6; price = 500000; };
+class O_APC_Tracked_02_cannon_F { quality = 4; price = 500000; };
+class O_APC_Wheeled_02_rcws_F { quality = 3; price = 250000; };
+class O_MBT_02_arty_F { quality = 6; price = 5000000; };
+class O_MBT_02_cannon_F { quality = 6; price = 5000000; };
+class pook_SCUD_OPFOR { quality = 6; price = 9000000; };
+class pook_SCUD_BLUFOR { quality = 6; price = 9000000; };
+class pook_SCUD_INDFOR { quality = 6; price = 9000000; };
+class pook_3K60_OPFOR { quality = 6; price = 6000000; };
+class pook_3K60_BLUFOR { quality = 6; price = 6000000; };
+class pook_3K60_INDFOR { quality = 6; price = 6000000; };
+class pook_4K51_OPFOR { quality = 6; price = 6000000; };
+class pook_4K51_BLUFOR { quality = 6; price = 6000000; };
+class pook_4K51_INDFOR { quality = 6; price = 6000000; };
+class pook_9K52_OPFOR { quality = 6; price = 6000000; };
+class pook_9K52_BLUFOR { quality = 6; price = 6000000; };
+class pook_9K52_INDFOR { quality = 6; price = 6000000; };
+class pook_9K57_OPFOR { quality = 6; price = 6000000; };
+class pook_9K57_BLUFOR { quality = 6; price = 6000000; };
+class pook_9K57_INDFOR { quality = 6; price = 6000000; };
+class pook_9K57M_OPFOR { quality = 6; price = 6000000; };
+class pook_9K57M_BLUFOR { quality = 6; price = 6000000; };
+class pook_9K57M_INDFOR { quality = 6; price = 6000000; };
+class pook_9K58_OPFOR { quality = 6; price = 6000000; };
+class pook_9K58_BLUFOR { quality = 6; price = 6000000; };
+class pook_9K58_INDFOR { quality = 6; price = 6000000; };
+//class pook_9K720K_OPFOR							{ quality = 6; price = 6000000; }; 
+//class pook_9K720K_BLUFOR							{ quality = 6; price = 6000000; };
+//class pook_9K720K_INDFOR							{ quality = 6; price = 6000000; }; 
+//class pook_9K720_OPFOR							{ quality = 6; price = 6000000; };
+//class pook_9K720_BLUFOR							{ quality = 6; price = 6000000; };
+//class pook_9K720_INDFOR							{ quality = 6; price = 6000000; };
+class pook_A222_OPFOR { quality = 6; price = 6000000; };
+class pook_A222_BLUFOR { quality = 6; price = 6000000; };
+class pook_A222_INDFOR { quality = 6; price = 6000000; };
+class I_Truck_02_MRL_F { quality = 6; price = 6000000; };  //zamak arty
+class CUP_B_RM70_CZ { quality = 6; price = 6000000; }   // MR-70 ACR Artyna
+class CUP_B_M1128_MGS_Desert { quality = 4; price = 700000; };
+class CUP_B_M1128_MGS_Desert_Slat { quality = 4; price = 700000; };
+class CUP_B_M1128_MGS_Woodland { quality = 4; price = 700000; };
+class CUP_B_M1128_MGS_Woodland_Slat { quality = 4; price = 700000; };
+class CUP_B_M60A3_USMC { quality = 4; price = 800000; };
+class CUP_B_M60A3_TTS_USMC { quality = 4; price = 800000; };
+class CUP_I_M60A3_RACS { quality = 4; price = 800000; };
+class CUP_I_M60A3_TTS_RACS { quality = 4; price = 800000; };
+class CUP_O_BMP3_RU { quality = 4; price = 400000; };
+class CUP_O_BMP3_CSAT_T { quality = 4; price = 400000; };
+
+///////////////////////////////////////////////////////////////////////////////
+// Tracked Armed
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// Tanks
+///////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Artillery
+///////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Arma 3 Boats
+///////////////////////////////////////////////////////////////////////////////
+class B_Boat_Armed_01_minigun_F { quality = 1; price = 20000; };
+class B_Boat_Transport_01_F { quality = 1; price = 700; };
+class B_G_Boat_Transport_01_F { quality = 1; price = 700; };
+class B_Lifeboat { quality = 1; price = 700; };
+class B_SDV_01_F { quality = 1; price = 1000; };
+class C_Boat_Civil_01_F { quality = 1; price = 700; };
+class C_Boat_Civil_01_police_F { quality = 1; price = 700; };
+class C_Boat_Civil_01_rescue_F { quality = 1; price = 700; };
+class C_Rubberboat { quality = 1; price = 700; };
+class I_Boat_Armed_01_minigun_F { quality = 1; price = 10000; };
+class I_Boat_Transport_01_F { quality = 1; price = 700; };
+class I_G_Boat_Transport_01_F { quality = 1; price = 700; };
+class I_SDV_01_F { quality = 1; price = 1000; };
+class O_Boat_Armed_01_hmg_F { quality = 1; price = 10000; };
+class O_Boat_Transport_01_F { quality = 1; price = 700; };
+class O_G_Boat_Transport_01_F { quality = 1; price = 700; };
+class O_Lifeboat { quality = 1; price = 700; };
+class O_SDV_01_F { quality = 1; price = 1000; };
+
+///////////////////////////////////////////////////////////////////////////////
+// Arma 3 UAV and UAG
+///////////////////////////////////////////////////////////////////////////////
+class B_UAV_01_F { quality = 3; price = 20000; };
+class B_UAV_02_CAS_F { quality = 3; price = 50000; };
+class B_UAV_02_F { quality = 3; price = 30000; };
+class B_UGV_01_F { quality = 3; price = 20000; };
+class B_UGV_01_rcws_F { quality = 3; price = 100000; };
+class I_UAV_01_F { quality = 3; price = 20000; };
+class I_UAV_02_CAS_F { quality = 3; price = 50000; };
+class I_UAV_02_F { quality = 3; price = 30000; };
+class I_UGV_01_F { quality = 3; price = 20000; };
+class I_UGV_01_rcws_F { quality = 3; price = 100000; };
+class O_UAV_01_F { quality = 3; price = 20000; };
+class O_UAV_02_CAS_F { quality = 3; price = 50000; };
+class O_UAV_02_F { quality = 3; price = 30000; };
+class O_UGV_01_F { quality = 3; price = 20000; };
+class O_UGV_01_rcws_F { quality = 3; price = 100000; };
+class B_UAV_02_dynamicLoadout_F { quality = 6; price = 100000; };		// MQ-4A Greyhawk
+class O_UAV_02_dynamicLoadout_F { quality = 6; price = 100000; };		// K40 Ababil-3
+class I_UAV_02_dynamicLoadout_F { quality = 6; price = 100000; };		// K40 Ababil-3
+
+///////////////////////////////////////////////////////////////////////////////
+// Arma 3 Civ Helicopters
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// Arma 3 Unarmed Helicopters
+///////////////////////////////////////////////////////////////////////////////
+class B_Heli_Light_01_F { quality = 1; price = 22000; };
+class B_Heli_Light_01_stripped_F { quality = 1; price = 22000; };
+class B_Heli_Transport_03_unarmed_F { quality = 1; price = 40000; };
+class B_Heli_Transport_03_unarmed_green_F { quality = 1; price = 40000; };
+class C_Heli_Light_01_civil_F { quality = 1; price = 22000; };
+class C_Heli_light_01_blueLine_F { quality = 1; price = 22000; };
+class C_Heli_light_01_blue_F { quality = 1; price = 22000; };
+class C_Heli_light_01_digital_F { quality = 1; price = 22000; };
+class C_Heli_light_01_elliptical_F { quality = 1; price = 22000; };
+class C_Heli_light_01_furious_F { quality = 1; price = 22000; };
+class C_Heli_light_01_graywatcher_F { quality = 1; price = 22000; };
+class C_Heli_light_01_ion_F { quality = 1; price = 22000; };
+class C_Heli_light_01_jeans_F { quality = 1; price = 22000; };
+class C_Heli_light_01_light_F { quality = 1; price = 22000; };
+class C_Heli_light_01_luxe_F { quality = 1; price = 22000; };
+class C_Heli_light_01_red_F { quality = 1; price = 22000; };
+class C_Heli_light_01_shadow_F { quality = 1; price = 22000; };
+class C_Heli_light_01_sheriff_F { quality = 1; price = 22000; };
+class C_Heli_light_01_speedy_F { quality = 1; price = 22000; };
+class C_Heli_light_01_stripped_F { quality = 1; price = 22000; };
+class C_Heli_light_01_sunset_F { quality = 1; price = 22000; };
+class C_Heli_light_01_vrana_F { quality = 1; price = 22000; };
+class C_Heli_light_01_wasp_F { quality = 1; price = 22000; };
+class C_Heli_light_01_wave_F { quality = 1; price = 22000; };
+class I_Heli_light_03_unarmed_F { quality = 1; price = 22000; };
+class O_Heli_Light_02_unarmed_F { quality = 1; price = 22000; };
+class O_Heli_Transport_04_F { quality = 1; price = 40000; };
+class O_Heli_Transport_04_ammo_F { quality = 1; price = 60000; };
+class O_Heli_Transport_04_ammo_black_F { quality = 1; price = 58000; };
+class O_Heli_Transport_04_bench_F { quality = 1; price = 40000; };
+class O_Heli_Transport_04_bench_black_F { quality = 1; price = 40000; };
+class O_Heli_Transport_04_black_F { quality = 1; price = 40000; };
+class O_Heli_Transport_04_box_F { quality = 1; price = 40000; };
+class O_Heli_Transport_04_box_black_F { quality = 1; price = 40000; };
+class O_Heli_Transport_04_covered_F { quality = 1; price = 48000; };
+class O_Heli_Transport_04_covered_black_F { quality = 1; price = 48000; };
+class O_Heli_Transport_04_fuel_F { quality = 1; price = 58000; };
+class O_Heli_Transport_04_fuel_black_F { quality = 1; price = 58000; };
+class O_Heli_Transport_04_medevac_F { quality = 1; price = 48000; };
+class O_Heli_Transport_04_medevac_black_F { quality = 1; price = 48000; };
+class O_Heli_Transport_04_repair_F { quality = 1; price = 53000; };
+class O_Heli_Transport_04_repair_black_F { quality = 1; price = 58000; };
+
+///////////////////////////////////////////////////////////////////////////////
+// Arma 3 Armed Helicopters
+///////////////////////////////////////////////////////////////////////////////
+class B_Heli_Attack_01_F { quality = 2; price = 160000; };
+class B_Heli_Attack_01_dynamicLoadout_F { quality = 2; price = 160000; };	//new dynamic version
+class B_Heli_Light_01_armed_F { quality = 2; price = 110000; };
+class B_Heli_Light_01_dynamicLoadout_F { quality = 2; price = 110000; };	//new dynamic version
+class B_Heli_Transport_01_F { quality = 2; price = 160000; };
+class B_Heli_Transport_01_camo_F { quality = 2; price = 160000; };
+class B_Heli_Transport_03_F { quality = 2; price = 160000; };
+class B_Heli_Transport_03_black_F { quality = 2; price = 160000; };
+class I_Heli_Transport_02_F { quality = 2; price = 160000; };
+class I_Heli_light_03_F { quality = 2; price = 110000; };
+class I_Heli_light_03_dynamicLoadout_F { quality = 2; price = 110000; };	//new dynamic version
+class O_Heli_Attack_02_F { quality = 2; price = 160000; };
+class O_Heli_Attack_02_black_F { quality = 2; price = 160000; };
+class O_Heli_Attack_02_dynamicLoadout_F { quality = 2; price = 160000; };	//new dynamic version
+class O_Heli_Attack_02_dynamicLoadout_black_F { quality = 2; price = 160000; };	//new dynamic version
+class O_Heli_Light_02_F { quality = 2; price = 110000; };
+class O_Heli_Light_02_dynamicLoadout_F { quality = 2; price = 110000; };
+class O_Heli_Light_02_v2_F { quality = 2; price = 110000; };
+
+///////////////////////////////////////////////////////////////////////////////
+// Arma 3 Civ Planes
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// Arma 3 Planes
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// Arma 3 Armed Planes
+///////////////////////////////////////////////////////////////////////////////
+class B_Plane_CAS_01_F { quality = 2; price = 550000; };
+class B_Plane_CAS_01_dynamicLoadout_F { quality = 2; price = 550000; };	//new dynamic version
+class GNT_C185 { quality = 2; price = 10000; };
+class GNT_C185F { quality = 2; price = 10000; };
+class GNT_C185T { quality = 2; price = 30000; };
+class I_Plane_Fighter_03_AA_F { quality = 2; price = 550000; };
+class I_Plane_Fighter_03_CAS_F { quality = 2; price = 550000; };
+class I_Plane_Fighter_03_dynamicLoadout_F { quality = 2; price = 550000; };	//new dynamic version
+class O_Plane_CAS_02_F { quality = 2; price = 550000; };
+class O_Plane_CAS_02_dynamicLoadout_F { quality = 2; price = 550000; };	//new dynamic version
+
+///////////////////////////////////////////////////////////////////////////////
+// Arma 3 Taru Pod - NOT LISTED IN TRADERS
+///////////////////////////////////////////////////////////////////////////////
+class Land_Pod_Heli_Transport_04_bench_F { quality = 3; price = 10000; };	// Taru Bench Pod
+class Land_Pod_Heli_Transport_04_covered_F { quality = 3; price = 10000; };	// Taru Transport Pod
+class Land_Pod_Heli_Transport_04_medevac_F { quality = 3; price = 10000; };	// Taru Medical Pod
+class Land_Pod_Heli_Transport_04_bench_black_F { quality = 3; price = 10000; };	// Taru Bench Pod
+class Land_Pod_Heli_Transport_04_covered_black_F { quality = 3; price = 10000; };	// Taru Transport Pod
+class Land_Pod_Heli_Transport_04_medevac_black_F { quality = 3; price = 10000; };	// Taru Medical Pod
--- TraderTool/TraderTool/Program.cs
+++ TraderTool/TraderTool/Program.cs
@@ -6,7 +6,7 @@ namespace TraderTool
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
